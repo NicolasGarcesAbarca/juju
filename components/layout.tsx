83d39a0cc3149d904f1f4b8668f5cc7a935d0bc9
nico <nickogarces@gmail.com>
2022-04-22 21:56:40 -0400

    new readme

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
+import React from 'react'
 
-const Layout: NextPage = ({ children }:any) => {
+const Layout = ({ children }: any) => {
     return (<div>{children}</div>)
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
+
+//Home:React.FC
 const Home: NextPage = () => {
   return (
   <div>home here </div>
